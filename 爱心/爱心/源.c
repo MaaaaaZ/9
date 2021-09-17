d5965c89019c7f5810d551a015501ba2caf8af93
MaaaaaZ <90545124+MaaaaaZ@users.noreply.github.com>
2021-09-17 20:35:05 +0800

    心♥
    心♥

---
+++ 爱心/爱心/源.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main() 
+{
+    printf("                                ooooooooooo                      ooooooooooo                            ");
+    printf("                             ooooooooooooooo                    oooooooooooooo                          ");
+    printf("                            oooooooooooooooooo                ooooooooooooooooo                         ");
+    printf("                          oooooooooooooooooooooo            ooooooooooooooooooooo                       ");
+    printf("                         oooooooooooooooooooooooo          ooooooooooooooooooooooo                      ");
+    printf("                        oooooooooooooooooooooooooo        ooooooooooooooooooooooooo                     ");
+    printf("                        ooooooooooooooooooooooooooo      oooooooooooooooooooooooooo                     ");
+    printf("                       oooooooooooooooooooooooooooo      ooooooooooooooooooooooooooo                    ");
+    printf("                       ooooooooooooooooooooooooooooo    oooooooooooooooooooooooooooo                    ");
+    printf("                       oooooooooooooooooooooooooooooo  ooooooooooooooooooooooooooooo                    ");
+    printf("                       ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo                    ");
+    printf("                        ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo                     ");
+    printf("                        oooooooooooooooooooooooooooooooooooooooooooooooooooooooooo                      ");
+    printf("                         oooooooooooooooooooooooooooooooooooooooooooooooooooooooo                       ");
+    printf("                          oooooooooooooooooooooooooooooooooooooooooooooooooooooo                        ");
+    printf("                            oooooooooooooooooooooooooooooooooooooooooooooooooo                          ");
+    printf("                             oooooooooooooooooooooooooooooooooooooooooooooooo                           ");
+    printf("                              ooooooooooooooooooooooooooooooooooooooooooooo                             ");
+    printf("                                ooooooooooooooooooooooooooooooooooooooooo                               ");
+    printf("                                  ooooooooooooooooooooooooooooooooooooo                                 ");
+    printf("                                    ooooooooooooooooooooooooooooooooo                                   ");
+    printf("                                       ooooooooooooooooooooooooooo                                      ");
+    printf("                                          ooooooooooooooooooooo                                         ");
+    printf("                                              oooooooooooooo                                            ");
+    printf("                                                ooooooooo                                               ");
+    printf("                                                   oooo                                                 ");
+    system("Pause");
+}    
